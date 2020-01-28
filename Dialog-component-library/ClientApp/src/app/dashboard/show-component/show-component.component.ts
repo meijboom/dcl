f2be32593bc54cce3625c2315b438ff20c7d5da9
Dennis <Dennis.meijboom@hotmail.com>
2020-01-28 10:21:49 +0100

    DialogCompLib on .NET

---
+++ Dialog-component-library/ClientApp/src/app/dashboard/show-component/show-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-component',
+  templateUrl: './show-component.component.html',
+  styleUrls: ['./show-component.component.scss']
+})
+export class ShowComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
