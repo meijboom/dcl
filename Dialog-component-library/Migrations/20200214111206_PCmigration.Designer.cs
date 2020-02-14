bef0fe63b079463d07a93211dcab47abe0f78a93
Dennis Meijboom <Dennis.meijboom@hotmail.com>
2020-02-14 12:24:31 +0100

    added migration on PC, local DB

--- Dialog-component-library/Migrations/20200214111206_PCmigration.Designer.cs
+++ Dialog-component-library/Migrations/20200214111206_PCmigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Dialog_component_library.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Dialog_component_library.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200214111206_PCmigration")]
+    partial class PCmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dialog-component-library/Migrations/20200214111206_PCmigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dialog_component_library.Migrations
+{
+    public partial class PCmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Components_ComponentId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ComponentId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ComponentId",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ComponentId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ComponentId",
+                table: "Users",
+                column: "ComponentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Components_ComponentId",
+                table: "Users",
+                column: "ComponentId",
+                principalTable: "Components",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
